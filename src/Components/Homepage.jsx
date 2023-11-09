2ab20a1d5fc6fba7cb5a0bf7ad9e0e4b1cbe615e
Patiencemugambi <patiencemugambi@gmail.com>
2023-11-09 07:53:29 +0300

    FAQs

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -149,7 +149,9 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         <div> 
           <div>
           <h2>Have any Questions?</h2>
-          <p>If you have inquiries for the Jiseti team or an interest in <br /> joining our mission to combat corruption in Africa, don't hesitate to get in touch!</p>
+          <p>If you have inquiries for the Jiseti team or an interest in  joining our mission to combat corruption in Africa, don't hesitate to get in touch!</p>
+          <a href="/contact" class="report-link">Contact Us</a>
+
           </div>
           <div>
             <img src="https://previews.123rf.com/images/argus456/argus4561605/argus456160522386/57141285-stop-corruption-3d-rendering-red-sticker-with-white-text.jpg" alt="" />
@@ -158,7 +160,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         <div class='faq-questions'>
           <h2>FAQs</h2>
               <p className="user-profile" onClick={toggleUserProfile}>
-                <p>1. What is Jiseti</p>
+                <h1>1. What is Jiseti</h1>
 
             {showUserProfile && (
               <div className="faq-expanded">
@@ -166,51 +168,9 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
               </div>
             )}
           </p>
-          <p className="user-profile" onClick={toggleUserProfile}>
-                <p>2. How can I create an account on Jiseti?</p>
 
-            {showUserProfile && (
-              <div className="faq-expanded">
-                <p>Jiseti is a platform designed to combat corruption in Africa by allowing citizens to report incidents and request government intervention in various issues. It empowers users to contribute to transparency and accountability.</p>                
-              </div>
-            )}
-          </p>
-          <p className="user-profile" onClick={toggleUserProfile}>
-                <p>3. What are Red Flags?</p>
 
-            {showUserProfile && (
-              <div className="faq-expanded">
-                <p>Jiseti is a platform designed to combat corruption in Africa by allowing citizens to report incidents and request government intervention in various issues. It empowers users to contribute to transparency and accountability.</p>                
-              </div>
-            )}
-          </p>
-          <p className="user-profile" onClick={toggleUserProfile}>
-                <p>4. What are Intervention Records?</p>
 
-            {showUserProfile && (
-              <div className="faq-expanded">
-                <p>Jiseti is a platform designed to combat corruption in Africa by allowing citizens to report incidents and request government intervention in various issues. It empowers users to contribute to transparency and accountability.</p>                
-              </div>
-            )}
-          </p>
-          <p className="user-profile" onClick={toggleUserProfile}>
-                <p>5. How do I add geolocation to my records?</p>
-
-            {showUserProfile && (
-              <div className="faq-expanded">
-                <p>Jiseti is a platform designed to combat corruption in Africa by allowing citizens to report incidents and request government intervention in various issues. It empowers users to contribute to transparency and accountability.</p>                
-              </div>
-            )}
-          </p>
-          <p className="user-profile" onClick={toggleUserProfile}>
-                <p>6. Can I change the geolocation of a record once it's submitted?</p>
-
-            {showUserProfile && (
-              <div className="faq-expanded">
-                <p>Jiseti is a platform designed to combat corruption in Africa by allowing citizens to report incidents and request government intervention in various issues. It empowers users to contribute to transparency and accountability.</p>                
-              </div>
-            )}
-          </p>
           
           
 </div>
